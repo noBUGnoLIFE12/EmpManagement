eb46fd6370c5b0d78e4decdf23b86493535aa4dd
noBUGnoLIFE12 <13021193585@163.com>
2022-05-24 09:48:06 +0900

    with lots of bugs

---
+++ src/main/java/com/emp/empmanagement/mapper/MemberMapper.java
@@ -0,0 +1,56 @@
+package com.emp.empmanagement.mapper;
+
+
+import com.emp.empmanagement.bean.Member;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface MemberMapper {
+/**
+ * 通过ID查询单条数据
+ */
+    Member queryById(Integer id);
+    @Select("SELECT * FROM member WHERE name=#{name} AND gender=#{gender} AND age=#{age} AND phone_num=#{phone_num} " +
+            "AND address=#{address}")
+    List<Member> queryByCondition(@Param("name")String name, @Param("age")Integer age, @Param("gender")String gender,
+                                  @Param("phone_num")String phone_num, @Param("address")String address);
+ /**
+ * 查询指定行数据
+ * @param offset 查询起始位置
+ * @param limit 查询条数
+ * @return 对象列表
+ */
+List<Member> queryAllByLimit(@Param("offset") int offset, @Param("limit") int limit);
+
+/**
+ * 通过实体作为筛选条件查询
+ *
+ * @param
+ * @return 对象列表
+ */
+@Select("SELECT * FROM member")
+List<Member> queryAll();
+
+/**
+ * 新增数据
+ *
+ * @param member 实例对象
+ * @return 影响行数
+ */
+@Insert("INSERT INTO member(name, age, gender, phone_num, address) VALUES(#{name}, #{age}, #{gender}, #{phone_num}, #{address})")
+int insert(Member member);
+
+/**
+ * 修改数据
+ */
+@Update("UPDATE member SET name=#{name}, age=#{age}, gender=#{gender}, phone_num=#{phone_num}, address=#{address} WHERE" +
+        "id=#{id}")
+int update(Member member);
+
+/**
+ * 删除数据
+ */
+int deleteById(Integer id);
+}
--- src/main/java/com/emp/empmanagement/service/impl/MemberServiceImpl.java
+++ src/main/java/com/emp/empmanagement/service/impl/MemberServiceImpl.java
@@ -9,5 +9,40 @@ import javax.annotation.Resource;
 @Service("MemberService")
 public class MemberServiceImpl implements MemberService {
     @Resource
-    private MemberDao memberDao;
+    private MemberMapper memberMapper;
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param  id
+     * @return 实例对象
+     */
+    @Override
+    public Member queryById(Integer id){
+        return this.memberMapper.queryById(id);
+    }
+    /**
+     * 查询多条数据
+     *
+     * @param offset 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    @Override
+    public List<Member> queryAllByLimit(int offset, int limit){
+        return this.memberMapper.queryAllByLimit(offset, limit);
+    }
+    @Override
+    public List<Member> queryByCondition(String name, Integer age, String gender, String phone_num, String address){
+        if(!name.isEmpty() && !gender.isEmpty() && age!=null && !phone_num.isEmpty() && !address.isEmpty()){
+        }return this.memberMapper.queryByCondition(name,age,gender,phone_num,address);
+    }
+    /**
+     * 新增数据
+     */
+    @Override
+    public Member insert(Member member){
+        this.memberMapper.insert(member);
+        return member;
+
+    }
 }
